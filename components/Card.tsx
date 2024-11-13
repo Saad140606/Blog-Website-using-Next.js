ce05d4de8ca1ca869e1e7bfdd1e78c718aad59ce
Saad140606 <syedsaadnajam2006@gmail.com>
2024-11-13 13:59:50 +0500

    first commit

--- components/Card.tsx
+++ components/Card.tsx
@@ -23,11 +23,12 @@ export default function Card({ category, title, image, price, link }: CardProps)
           height={1000}
         /></Link></div>
       
-      <div className="p-6">
-        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{category}</h3>
-        <h2 className="text-gray-900 title-font text-lg font-medium">{title}</h2>
+      <div className="p-6"><Link href={link}>
+      <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{category}</h3>
+        <h2 className="text-gray-900 title-font text-lg font-medium">{title}</h2></Link>
+        
        <div className="mt-1 text-gray-900 font-bold text-xl flex items-center">
-          AUTHOR: {price}
+         <Link href={link}>AUTHOR: {price}</Link>
           <Link href="#" className="ml-2 text-gray-500">
           <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" strokeWidth={2}>
               <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h6m-3-3l3 3-3 3" />
